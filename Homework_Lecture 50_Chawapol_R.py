877397a4b5b3835314ba31f44c880463615bef77
imbabaa <57980775+imbabaa@users.noreply.github.com>
2019-11-25 23:44:09 +0700

    Born to Dev

---
+++ Homework_Lecture 50_Chawapol_R.py
@@ -0,0 +1,15 @@
+def plusNumber(x,y):
+    print(x+y)
+plusNumber(10,20)
+
+def negativeNumber(x,y):
+    print(x-y)
+negativeNumber(30,20)
+
+def multiplyNumber(x,y):
+    print(x*y)
+multiplyNumber(2,50)
+
+def divideNumber(x,y):
+    print(x/y)
+divideNumber(10,2)
